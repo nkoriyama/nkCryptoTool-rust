c7adc31f85bd342f89181fd6eb5d54f4ad839340
Naohiro KORIYAMA <nkoriyama@gmail.com>
2026-04-26 13:54:40 +0900

    docs: 全ソースファイルにライセンスヘッダーを追加

--- src/processor.rs
+++ src/processor.rs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024-2026 Naohiro KORIYAMA <nkoriyama@gmail.com>
+ *
+ * This file is part of nkCryptoTool.
+ */
+
 use crate::config::{CryptoConfig, CryptoMode, Operation};
 use crate::error::{CryptoError, Result};
 use crate::strategy::CryptoStrategy;
--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024-2026 Naohiro KORIYAMA <nkoriyama@gmail.com>
+ *
+ * This file is part of nkCryptoTool.
+ */
+
 use std::process::Command;
 use std::fs;
 use std::path::Path;
